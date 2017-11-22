02070142ecf8492426e79f0510e5614367f6ee39
Сергей Бычков <bychkov.sergei.vladimirovich@gmail.com>
2017-11-22 12:12:47 +0300

    use queues

--- index.js
+++ index.js
@@ -1,67 +1,82 @@
 import redis from 'redis';
 import uuid from 'uuid';
 
+let defaultOpts = {
+    scope: 'rpc',
+    redis: {
+        host: 'localhost',
+        port: 6379,
+    }
+}
 
 export default class RPCClient {
-    constructor(redisConnectionOpts) {
-        this.redisConnectionOpts = redisConnectionOpts
+    constructor(opts) {
+        let _opts = {...defaultOpts, opts};
+        this._id = uuid.v4();
+        this.scope = _opts.scope;
+        this.redis = redis.createClient(_opts.redis);
         this.callbacks = {};
         this.cancels = {};
+
         this.init();
     }
 
-    init() {
-        this.sub = redis.createClient(this.redisConnectionOpts);
-
-        this.sub.on('message', (channel, msg) => {
-            this.callbacks[channel] && this.callbacks[channel](msg);
-        });
-
-        this.pub = this.sub.duplicate();
+    _getQueue(queue) {
+        return this.scope + ':' + queue;
     }
 
-    listen(channel, callback = () => {}) {
-        this.callbacks[channel] = callback;
-        this.sub.subscribe(channel);
-    }
+    on(queue, callback = (method, params) => {}) {
+        this.cancels[queue] = null;
+        let processQueue = this._getQueue(queue) + ':proc:' + this._id;
+        this.redis.brpoplpush(this._getQueue(queue), processQueue, (err, req) => {
 
-    removeListener(channel) {
-        delete this.callbacks[channel];
-        this.sub.unsubscribe(channel);
-    }
-
-    call(channel, msg, _id) {
-        return new Promise((resolve, reject) => {
-            _id = _id || uuid.v4();
-            this.callbacks[_id] = response => {
-                delete this.callbacks[_id];
-                delete this.cancels[_id];
-                resolve(response);
-            };
-            this.cancels[_id] = () => {
-                delete this.callbacks[_id];
-                delete this.cancels[_id];
-                reject({isCanceled: true});
-            };
-            this.sub.subscribe(_id);
-            this.pub.publish(channel, JSON.stringify({msg, _id}));
-        });
-    }
-
-    cancel(_id) {
-        this.cancels[_id] && this.cancels[_id]();
-    }
-
-    cancelableCall(channel, msg) {
-        const _id = uuid.v4();
-
-        return {
-            promise: () => {
-                return this.call(channel, msg, _id);
-            },
-            cancel() {
-                this.cancel(_id);
+            let respond = res => {
+                let response = {
+                    jsonrpc: '2.0',
+                    id: req.id,
+                    ...res
+                };
+                this.redis.lpush(this._getQueue(req.id), JSON.stringify(response));
+                this.redis.rpop(processQueue);
+                this.cancels[queue] = setTimeout(() => {
+                    this.processQueue(queue, callback);
+                }, 0);
             }
-        }
+
+            callback(req.method, req.params).then(result => {
+                respond({result});
+            }).catch(error => {
+                respond({error});
+            });
+        });
+    }
+
+    stop(queue) {
+        clearTimeout(this.cancels[queue]);
+    }
+
+    call(queue, method, params) {
+        return new Promise((resolve, reject) => {
+            let request = {
+                jsonrpc: '2.0',
+                id: uuid.v4(),
+                method: method,
+                params: params
+            }
+            this.redis.lpush(this._getQueue(queue), JSON.stringify(request));
+            this.redis.brpop(this._getQueue(request.id), 600, (err, res) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    let resData = JSON.parse(res);
+                    if (resData.error) {
+                        reject(err);
+                    } else {
+                        resolve(resData.result);
+                    }
+                }
+            });
+        });
+        
     }
 }
